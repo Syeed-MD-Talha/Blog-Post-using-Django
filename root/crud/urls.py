d97f399ac943a9ace8ed1d2d0dcc409138d605f8
Syeed-MD-Talha <62591318+Syeed-MD-Talha@users.noreply.github.com>
2024-06-18 15:54:36 +0600

    Adding other stuffs

--- root/crud/urls.py
+++ root/crud/urls.py
@@ -4,10 +4,10 @@ from .import views
 urlpatterns = [
     path('',views.home,name='blog_post'),
     path('create/',views.create,name='create_post'),
+    path('edit/<int:id>/',views.edit,name='edit_post'),
+    path('delete/<int:id>/',views.delete,name='delete_post'),
 ]
 
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- root/crud/views.py
+++ root/crud/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .forms import CreatePost
 from .models import BlogPost
 
@@ -27,3 +27,27 @@ def create(request):
         return render(request,'create.html',{'form':frm})
     
 
+
+# ============================   Editing Post ======================
+def edit(request,id):
+    post=get_object_or_404(BlogPost,id=id)
+
+    if request.method=='POST':
+        form=CreatePost(request.POST,request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_post')
+        else:
+            return render(request,'edit_post',{'frm':form})
+    
+    return render(request,'edit.html',{'form':CreatePost(instance=post),'id':id})
+
+
+
+
+# ============================  Deleting Post ======================
+
+def delete(request,id):
+    post=get_object_or_404(BlogPost,id=id)
+    post.delete()
+    return redirect('blog_post')
